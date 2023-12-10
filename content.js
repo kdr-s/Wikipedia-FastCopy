bf22348110c2d188ed9e52754a5cd0ab5f5a643f
kdr-s <31813475+kdr-s@users.noreply.github.com>
2023-12-11 01:48:18 +0900

    リファクタリング

--- content.js
+++ content.js
@@ -1,66 +1,69 @@
 function handleKeydownEvent(event) {
-    if (event.key === 'c') {
-        // 入力要素から帰属テキストを取得
-        var attributionInput = document.getElementById('ooui-2');
-        var attributionText = attributionInput ? attributionInput.value : '';
-
-        // テキストと画像のURLを組み合わせる
-        var combinedText = "出典：" + attributionText;
-
-        // 組み合わせたテキストをクリップボードにコピー
-        navigator.clipboard.writeText(combinedText).then(function () {
-            console.log('Copied to clipboard successfully!');
-        }, function (err) {
-            console.error('Could not copy text: ', err);
-        });
-
-    } else if (event.key === 'd') {
-        // d キーが押されたときの処理
-        var detailsButton = document.querySelector('.mw-mmv-description-page-button');
-        if (detailsButton) {
-            // リンクのクリックイベントを発火させる
-            detailsButton.click();
-        } else {
-            console.log('詳細ボタンが見つかりませんでした。');
-        }
+    switch (event.key) {
+        case 'c':
+            copyAttributionText();
+            break;
+        case 'd':
+            clickDetailsButton();
+            break;
     }
 }
 
-function register(downloadButton) {
-    if (downloadButton) {
-        // 要素のクリックイベントを発火させる
-        downloadButton.click();
-        document.removeEventListener('keydown', handleKeydownEvent);
-        document.addEventListener('keydown', handleKeydownEvent);
-        // ポップアップを非表示にさせる
-        document.body.click()
+function copyAttributionText() {
+    var attributionInput = document.getElementById('ooui-2');
+    var attributionText = attributionInput ? attributionInput.value : '';
+    var combinedText = "出典：" + attributionText;
+
+    navigator.clipboard.writeText(combinedText).then(function () {
+        console.log('Copied to clipboard successfully!');
+    }, function (err) {
+        console.error('Could not copy text: ', err);
+    });
+}
+
+function clickDetailsButton() {
+    var detailsButton = document.querySelector('.mw-mmv-description-page-button');
+    if (detailsButton) {
+        detailsButton.click();
     } else {
-        console.log('ダウンロードボタンが見つかりませんでした。');
+        console.log('詳細ボタンが見つかりませんでした。');
     }
 }
 
-function unregister() {
+function registerKeydownListener() {
+    document.addEventListener('keydown', handleKeydownEvent);
+}
+
+function unregisterKeydownListener() {
     document.removeEventListener('keydown', handleKeydownEvent);
+}
+
+function clickCitation(citationButton) {
+    citationButton.click();
+    // ポップアップを非表示にさせる
+    document.body.click();
+}
+
+function processEventHandler(){
+    var citationButton = document.querySelector('.mw-mmv-download-button');
+    if (citationButton) {
+        clickCitation(citationButton);
+        registerKeydownListener();
+    } else {
+        unregisterKeydownListener()
+    }
+}
+
+function startExtension() {
+    processEventHandler();
+    observeDOMChanges();
 }
 
 function observeDOMChanges() {
     const observer = new MutationObserver(mutations => {
-        var downloadButton = document.querySelector('.mw-mmv-download-button');
-        if (downloadButton) {
-            register(downloadButton);
-        } else {
-            unregister();
-        }
+        processEventHandler();
     });
     observer.observe(document.body, { childList: true, subtree: true });
-}
-
-function startExtension() {
-    var downloadButton = document.querySelector('.mw-mmv-download-button');
-    if (downloadButton) {
-        register(downloadButton);
-    }
-    observeDOMChanges();
 }
 
 if (document.readyState === 'complete') {
